a168361fbe4f4b9a469607687de4921d36eee13b
Jamo Luhrsen <jluhrsen@gmail.com>
2025-04-20 17:42:26 -0700

    add basic US map
    also has ability to debug map coordinates
    
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
+import { useMapEvents } from 'react-leaflet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>College Football Map</h1>
+          <MapContainer
+              center={[41.4179, -118.8771]}
+              zoom={5.7}
+              scrollWheelZoom={true}
+              zoomSnap={0.15}
+              zoomDelta={0.15}
+              style={{ height: "90vh", width: "100%" }}
+          >
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <MapLogger /> */}
+        </MapContainer>
+      </div>
   );
+}
+
+function MapLogger() {
+    useMapEvents({
+        moveend: (e) => {
+            const map = e.target;
+            const center = map.getCenter();
+            const zoom = map.getZoom();
+            console.log(`Center: [${center.lat.toFixed(4)}, ${center.lng.toFixed(4)}], Zoom: ${zoom}`);
+        }
+    });
+    return null;
 }
 
 export default App;
