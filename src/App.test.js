fd8b1add4db63a03a4166fa1b98b13e794b2d601
Jamo Luhrsen <jluhrsen@gmail.com>
2025-12-27 10:08:49 -0800

    test: wrap App with context providers in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { GameDataProvider } from './contexts/GameDataContext';
+import { FilterProvider } from './contexts/FilterContext';
 
 test('renders college football map', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/college football map/i);
+  render(
+    <GameDataProvider>
+      <FilterProvider>
+        <App />
+      </FilterProvider>
+    </GameDataProvider>
+  );
+  const headingElement = screen.getByText(/football game map/i);
   expect(headingElement).toBeInTheDocument();
 });
